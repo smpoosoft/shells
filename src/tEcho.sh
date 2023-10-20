cd181a6f934f08d9cac6097bfbd9a9d71c16ba78
root <fanzouguo@gmail.com>
2023-10-21 05:39:22 +0800

    feat: 增加命名空间

--- src/tEcho.sh
+++ src/tEcho.sh
@@ -3,26 +3,26 @@
 # 格式化shell脚本的屏幕显示，如带颜色的输出，或预置颜色的输出。
 
 # 进行常规控制台打印信息
-function tlog() {
+function tEcho.log() {
 	echo "日志$1"
 }
 
 # 输出蓝色标题信息
-function tInfo() {
+function tEcho.info() {
 	echo "信息$1"
 }
 
 # 输出成功信息
-function tSucc() {
+function tEcho.succ() {
 	echo "成功$1"
 }
 
 # 输出警告信息
-function tWarn() {
+function tEcho.warn() {
 	echo "警告$1"
 }
 
 # 输出异常信息
-function tErr() {
+function tEcho.err() {
 	echo "错误$1"
 }
