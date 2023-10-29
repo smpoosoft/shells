1e1bdb488f0c9a53e10c8bc030e0de5610e4b76c
root <fanzouguo@gmail.com>
2023-10-30 00:03:46 +0800

    添加了以下å内容 注释文字的转义提示

--- mgr.sh
+++ mgr.sh
@@ -1,11 +1,11 @@
 source /smpoo_file/.lib/v2.0.0/src/tEcho.sh
 
 mgr() {
-	tEcho.info "请输入提交备注："
+	tEcho.info "请输入提交备注(备注中的引号用反斜杠转义)："
 	read commitMemo
 	if [ -n "${commitMemo}" ]; then
 		sudo git add .
-		sudo git commit -m ${commitMemo}
+		sudo git commit -m "${commitMemo}"
 		git push -u origin $(git rev-parse --abbrev-ref HEAD)
 	else
 		mgr
